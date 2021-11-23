98da4157dd487138123bf09d862c5fe3b31777b6
Maianne Thornton <maiannethornton@Maiannes-MBP.lan>
2021-11-23 15:15:27 -0500

    Added dynamic functionality so that the page is effortlessly kept up to date. Initial version of project complete. Added dynamic functionality to keep the site effortless updated. Initial version of project is complete.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,26 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+// dynamically sets up functionality so we only get unique categories from the list | creates an array (.map) that displays all of the categories then filters out duplicated categories using new Set
+const allCategories = ["all",...new Set(items.map((item) => item.category))];
+console.log(allCategories);
+
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
+  // using setMenuItems to change the list that is displayed when filtering by category
+  const filterItems = (category) => {
+    // if category = all set menuItems back to default items array
+    if (category === "all"){
+      setMenuItems(items);
+      return;
+    }
+    // if the item category property matches the string being passed in return that item to the newItems array
+    const newItems = items.filter((item)=> item.category === category)
+    // passing in the newItems prop to the setMenuItems array
+    setMenuItems(newItems)
+  };
+
   return (
     <main>
       <section className="menu section">
@@ -13,7 +30,12 @@ function App() {
           <h2>our menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories />
+        {/* Used in the manual method */}
+        {/* passing in the filterItems prop */}
+        {/* <Categories filterItems={filterItems}/> */}
+        {/* passing in the categories prop and the filterItems prop to use them  */}
+        <Categories categories={categories} filterItems={filterItems}/>
+
         {/* passing in the menuItems prop to access the data in the menu */}
         <Menu items={menuItems}/>
       </section>
--- src/Categories.js
+++ src/Categories.js
@@ -1,15 +1,32 @@
 import React from 'react';
 
-// manual approach
-const Categories = ({ filterItems }) => {
+// // manual approach
+// const Categories = ({ filterItems }) => {
+//   return (
+//     <div className="btn-container">
+//       <button className="filter-btn" onClick={() => filterItems("all")}>
+//         all
+//       </button>
+//       <button className="filter-btn" onClick={() => filterItems("breakfast")}>
+//         breakfast
+//       </button>
+//     </div>
+//   );
+// };
+// dynamic approach (in sync with the data, any changes added to the data.js is immediately represented on the page)
+// passing in the categories prop from app.js
+const Categories = ({ categories, filterItems }) => {
   return (
     <div className="btn-container">
-      <button className="filter-btn" onClick={() => filterItems("all")}>
-        all
-      </button>
-      <button className="filter-btn" onClick={() => filterItems("breakfast")}>
-        breakfast
-      </button>
+      {/* creating a new array and returning a button for each category */}
+      {categories.map((category, index) => {
+        return (
+          // onClick return only the items that match the category clicked
+          <button type="button" className="filter-btn" key="{index}" onClick={()=> filterItems(category)}>
+            {category}
+          </button>
+        )
+      })}
     </div>
   );
 };
